62c4e324e2febaa126ff46d48bbd63211a842c91
Arturo Vazquez <arturva.81@gmail.com>
2025-11-24 20:52:14 -0800

    update app.js

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
 $(function() {
-  // YOUR CODE HERE
-})
+  
+  $('#toggleButton').on('click', function() {
+    $('#myParagraph').toggle();
+  });
+
+  $('#colorButton').on('click', function() {
+    $('#colorDiv').css('background-color', 'yellow');
+  });
+
+  $('#addClassButton').on('click', function() {
+    $('#classDiv').addClass('New-Class');
+  });
+
+  $('#removeClassButton').on('click', function() {
+    $('#classDiv').removeClass('New-Class');
+  });
+
+  $('#fadeInButton').on('click', function() {
+    $('#fadeDiv').fadeIn();
+  });
+
+  $('#fadeOutButton').on('click', function() {
+    $('#fadeDiv').fadeOut();
+  });
+
+  $('#slideUpButton').on('click', function() {
+    $('#slideDiv').slideUp();
+  });
+
+  $('#slideDownButton').on('click', function() {
+    $('#slideDiv').slideDown();
+  });
+
+});
